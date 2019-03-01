314a5cd1182c8808b89da6f84ceaf1f37205b3f6
Kentarou Ueda <ueken0307@hotmail.co.jp>
2019-03-02 03:35:45 +0900

    [update]canvasクリア用の関数作成

--- draw.js
+++ draw.js
@@ -1,2 +1,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+function canvasReset(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.fillStyle = 'pink';
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+}
