abf6c5cd622bdaa1f4ff18fc2b7fdee00b16c1ec
Kentarou Ueda <ueken0307@hotmail.co.jp>
2019-03-03 04:34:46 +0900

    [update]タイマーの実装、スコア、正確さの計算の実装
    あとはデザインと音を流すぐらいですね

--- game.js
+++ game.js
@@ -9,6 +9,14 @@ var Game = function(param){
   this.natori = new Image();
   this.natori.src = 'natori.png';
 
+  this.isActive = false;
+  this.nowTime = 90;
+  
+  this.score = 0;
+  this.keyCount = 0;
+  this.missCount = 0;
+  this.accuracy = 100.0;
+
   this.draw = function(){
     canvasReset();
     let p=this.problems[this.pCount];
@@ -16,85 +24,130 @@ var Game = function(param){
     ctx.drawImage(this.syringe,0,0,1001,101,0,180,1000,100);
     ctx.drawImage(this.natori,0,0,800,800,700,280,200,200);
 
+    ctx.font = '35px serif';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'black'
+    ctx.fillText(this.nowTime,900,50);
+
+    ctx.fillText(this.score,600,50);
+    ctx.fillText(this.accuracy.toFixed(1) + '%',700,50);
+
     ctx.font = '24px serif';
     ctx.textAlign = 'left';
-    ctx.fillStyle = 'black';
-    ctx.fillText(p.text,80,220);
+    if(this.isActive){  
+      ctx.fillStyle = 'black';
+      ctx.fillText(p.text,80,220);
+      
+      ctx.fillStyle = 'gray';
+      ctx.fillText(p.getSelectText(),80,260);
+      ctx.fillStyle = 'black';
+      ctx.fillText(p.getNowText(),80,260); 
+    }else{
+      ctx.fillStyle = 'red';
+      ctx.textAlign = 'center';
+      if(this.nowTime > 0){
+        ctx.fillText('スペースキーでスタート',500,240);
+      }else{
+        ctx.fillText('おつかれさな〜',500,240);
+      }
+    }
+
     
-    ctx.fillStyle = 'gray';
-    ctx.fillText(p.getSelectText(),80,260);
-    ctx.fillStyle = 'black';
-    ctx.fillText(p.getNowText(),80,260);
   }
 
   this.update = function(){
+    if(this.isActive){
+      let date = new Date();
+      this.nowTime = 90 - Math.floor((date.getTime() - this.startTime) / 1000);
 
+      if(this.nowTime < 1){
+        this.isActive = false;
+        setTimeout(this.finish,2000); 
+      }
+    }
+  }
+  
+  this.finish = function(){
+    sm.changeScene(0,{});
   }
 
   this.keyPress = function(e){
-    let allowList = ['!','?','/','.',',','-','(',')']
-    const isAllow = function(key){
-      for(let i of allowList){if(i == key){return true;break;}}
-      return false;
-    }
-    
-    if((48 <= e.keyCode && e.keyCode <= 57) || (65 <= e.keyCode && e.keyCode <= 90) || (97 <= e.keyCode && e.keyCode <= 122) || isAllow(e.key)){
-            
-      let p = this.problems[this.pCount];
-      
-      //選択中の文字列であるかどうか
-      let check = function(num){
-        for(let i of p.selectList[p.tCount]){
-          if(i == num) return true;
-        }
+    if(this.isActive){ 
+      let allowList = ['!','?','/','.',',','-','(',')']
+      const isAllow = function(key){
+        for(let i of allowList){if(i == key){return true;break;}}
         return false;
       }
       
-      //選択中の文字列に文字と等しいかどうか
-      let tmpList = [];
-      for(let i=0;i<p.alphabet[p.tCount].length;++i){
-        if(check(i) && p.alphabet[p.tCount][i][p.sCount] == e.key){
-          tmpList.push(i);
-        }
-      }
-      
-      //該当する選択肢があるならそれを選択して次に進める
-      if(tmpList.length != 0){
-        p.sCount++;
-        p.selectList[p.tCount] = tmpList;
-        
-        //候補の中で、最後まで当てはまったやつがあるならその文字は終わり
-        for(let i of tmpList){
-          if(p.sCount == p.alphabet[p.tCount][i].length){
-            if(p.tCount+1 < p.alphabet.length){
-              p.tCount++;
-              p.sCount = 0;
-              //1文字終わり
-            }else{
-              p.sCount = 0;
-              //1文字終わり
+      if((48 <= e.keyCode && e.keyCode <= 57) || (65 <= e.keyCode && e.keyCode <= 90) || (97 <= e.keyCode && e.keyCode <= 122) || isAllow(e.key)){
               
-              p.tCount=0;
-              this.pCount++;
-              //1文終わり
-            }
-            break;
+        let p = this.problems[this.pCount];
+        this.keyCount++;
+        this.accuracy = ((this.keyCount - this.missCount)/this.keyCount)*100;
+        
+        //選択中の文字列であるかどうか
+        let check = function(num){
+          for(let i of p.selectList[p.tCount]){
+            if(i == num) return true;
+          }
+          return false;
+        }
+        
+        //選択中の文字列に文字と等しいかどうか
+        let tmpList = [];
+        for(let i=0;i<p.alphabet[p.tCount].length;++i){
+          if(check(i) && p.alphabet[p.tCount][i][p.sCount] == e.key){
+            tmpList.push(i);
           }
         }
-      }else{
-        //もし一つ前の文字がんで'n'許容の場合で、入力文字がnなら
-        if(p.tCount>0 && p.alphabet[p.tCount-1][p.selectList[p.tCount-1][0]]=='n' && e.key == 'n'){
-          //一つ前のんの選択を'nn'にする
-          p.selectList[p.tCount-1] = [];
-          for(let i=0;i<p.alphabet[p.tCount-1].length;++i){
-            if(p.alphabet[p.tCount-1][i] == 'nn'){
-              p.selectList[p.tCount-1].push(i);
+        
+        //該当する選択肢があるならそれを選択して次に進める
+        if(tmpList.length != 0){
+          p.sCount++;
+          p.selectList[p.tCount] = tmpList;
+          
+          //候補の中で、最後まで当てはまったやつがあるならその文字は終わり
+          for(let i of tmpList){
+            if(p.sCount == p.alphabet[p.tCount][i].length){
+              if(p.tCount+1 < p.alphabet.length){
+                p.tCount++;
+                p.sCount = 0;
+                //1文字終わり
+              }else{
+                p.sCount = 0;
+                //1文字終わり
+                
+                p.tCount=0;
+                this.score+=p.textH.length*100;
+                this.pCount++;
+                //1文終わり
+              }
               break;
             }
           }
         }else{
-          //当てはまりなし
+          //もし一つ前の文字がんで'n'許容の場合で、入力文字がnなら
+          if(p.tCount>0 && p.alphabet[p.tCount-1][p.selectList[p.tCount-1][0]]=='n' && e.key == 'n'){
+            //一つ前のんの選択を'nn'にする
+            p.selectList[p.tCount-1] = [];
+            for(let i=0;i<p.alphabet[p.tCount-1].length;++i){
+              if(p.alphabet[p.tCount-1][i] == 'nn'){
+                p.selectList[p.tCount-1].push(i);
+                break;
+              }
+            }
+          }else{
+            //当てはまりなし
+            this.missCount++;
+            this.accuracy = ((this.keyCount - this.missCount)/this.keyCount)*100;
+          }
         }
+      }
+    }else{
+      if(e.key == ' '){
+        this.isActive = true;
+        let date = new Date();
+        this.startTime = date.getTime();
       }
     }
   }
