4c9c22e74f9c23189887b09e86d9b47bef61312d
Kentarou Ueda <ueken0307@hotmail.co.jp>
2019-03-04 00:17:06 +0900

    [update]result画面のデザイン作成、ツイート機能の実装

--- result.js
+++ result.js
@@ -1,42 +1,50 @@
 var Result = function(param){
   this.finalScore = (param.score * (param.accuracy/100)).toFixed(0);
+  this.speed = ((param.keyCount - param.missCount)/90.0).toFixed(1);
 
   this.buttons = [
-    {x:20,y:200,width:500,height:50,font:'24px JKfont',textAlign:'center',mouseOver:0,
+    {x:20,y:300,width:500,height:50,font:'24px JKfont',textAlign:'center',mouseOver:0,
     bColor:'white',fColor:'black',bOverColor:'red',fOverColor:'white',
     label:'タイトルへ',
     onClick:function(){
       sm.changeScene('Select',{});
     }},
 
-    {x:20,y:260,width:500,height:50,font:'24px JKfont',textAlign:'center',mouseOver:0,
+    {x:20,y:360,width:500,height:50,font:'24px JKfont',textAlign:'center',mouseOver:0,
     bColor:'white',fColor:'black',bOverColor:'red',fOverColor:'white',
     label:'もう一度',
     onClick:function(){
       sm.changeScene('Game',{level:param.level});
     }},
 
-    {x:20,y:320,width:500,height:50,font:'24px JKfont',textAlign:'center',mouseOver:0,
+    {x:20,y:420,width:500,height:50,font:'24px JKfont',textAlign:'center',mouseOver:0,
     bColor:'DodgerBlue',fColor:'white',bOverColor:'DeepSkyBlue',fOverColor:'white',
     label:'結果をツイート',
     onClick:()=>{
       let url = "https://www.google.com";
-      let text = `${param.level} ${this.finalScore} (${param.accuracy.toFixed(1)}％) #test`;
-      window.open(`https://twitter.com/share?url=${url}&text=${text}`);
+      let text = `レベル${param.level+1}で${this.finalScore}点獲得！！ (正確率:${param.accuracy.toFixed(1)}％, 速さ:${this.speed}key/s)`;
+      window.open(`https://twitter.com/share?url=${url}&text=${text}&hashtags=名取さなのつよつよタイピング`);
     }}
   ];
 
   this.draw = function(){
     canvasReset();
 
+    ctx.drawImage(natori,0,0,natori.width,natori.height,600,80,400,400);
+
     ctx.font = '30px JKfont';
     ctx.textAlign = 'left';
     ctx.fillStyle = 'black';
-    ctx.fillText('基本スコア:' + param.score +  ' × 正確さ:' + param.accuracy.toFixed(1) + 
-    '%(' + (param.keyCount - param.missCount) + '/' +  param.keyCount + ')' ,10,50);
+    ctx.fillText('基本スコア : ' + param.score,20,50);
+    ctx.fillText('正確さ : ' + param.accuracy.toFixed(1) + 
+    '%(' + (param.keyCount - param.missCount) + '/' +  param.keyCount + ')',20,100);
+    ctx.fillText('スピード : ' + this.speed + 'key/s',20,150);
+
+    ctx.fillText('最終スコア : ' + param.score + ' × ' + param.accuracy + '%',20,200);
+    ctx.fillText('=',310,240);
     ctx.font = '50px JKfont';
     ctx.fillStyle = 'red';
-    ctx.fillText(this.finalScore,0,100);
+    ctx.fillText(  this.finalScore,350,250);
     
     for(let i of this.buttons){
       ctx.font = i.font;
