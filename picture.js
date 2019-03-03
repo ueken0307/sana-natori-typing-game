5ffc4da5fd1cdf4d421785b753f52e6fe62bacf1
Kentarou Ueda <ueken0307@hotmail.co.jp>
2019-03-03 23:22:50 +0900

    [add]picture.jsの追加、Imageオブジェクトの移動

---
+++ picture.js
@@ -0,0 +1,17 @@
+var syringe = new Image();
+syringe.src = 'assets/syringe.png';
+
+var natori = new Image();
+natori.src = 'assets/natori.png';
+
+var drug = new Image();
+drug.src = 'assets/drug.png';
+
+var clock = new Image();
+clock.src = 'assets/clock.png';
+
+var eggplant = new Image();
+eggplant.src = 'assets/eggplant.png';
+
+var keyboard = new Image();
+keyboard.src = 'assets/keyboard.png';
