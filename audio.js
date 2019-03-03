db12aff8843616cf9755e6bdda4cd9cb46f2f8c8
Kentarou Ueda <ueken0307@hotmail.co.jp>
2019-03-03 20:24:04 +0900

    [update]overSoundとdecideSoundを変更
    ふんふん...やっぴ〜！

--- audio.js
+++ audio.js
@@ -1,7 +1,7 @@
-var overSound = 'assets/se_maoudamashii_system26.mp3';
-var decideSound = 'assets/se_maoudamashii_system37.mp3';
+var overSound = 'assets/ふん1.mp3';
+var decideSound = 'assets/やっぴ～2.mp3';
 var okSound = 'assets/se_maoudamashii_system40.mp3';
-var ngSound = 'assets/se_maoudamashii_system39.mp3';
+var ngSound = 'assets/ｳｪｧｯ.mp3';
 
 var bgm = new Audio('assets/bgm_maoudamashii_8bit22.mp3');
 bgm.loop = true;
