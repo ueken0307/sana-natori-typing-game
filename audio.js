e365061179331fe7f36c80c51e89d06ab843f6dd
Kentarou Ueda <ueken0307@hotmail.co.jp>
2019-03-04 17:31:32 +0900

    [update]音声の音量調整

--- audio.js
+++ audio.js
@@ -1,13 +1,14 @@
-var overSound = 'assets/ふん1.mp3';
-var decideSound = 'assets/やっぴ～2.mp3';
-var okSound = 'assets/se_maoudamashii_system40.mp3';
-var ngSound = 'assets/ｳｪｧｯ.mp3';
+var overSound = {src:'assets/ふん1.mp3',volume:1.0};
+var decideSound = {src:'assets/やっぴ～2.mp3',volume:0.7};
+var okSound = {src:'assets/se_maoudamashii_system40.mp3',volume:0.6};
+var ngSound = {src:'assets/ｳｪｧｯ.mp3',volume:0.8};
 
 var bgm = new Audio('assets/bgm_maoudamashii_8bit22.mp3');
 bgm.loop = true;
-bgm.volume = 0.2;
+bgm.volume = 0.1;
 
-var playSE = function(src){
-  let tmp = new Audio(src);
+var playSE = function(target){
+  let tmp = new Audio(target.src);
+  tmp.volume = target.volume;
   tmp.play();
 }
